01c7cde8b8f9a615fd81cf60341d4d43a1352e4e
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-16 18:15:40 +0100

    Initial commit - Euklidscher Algorithmus - Informatik

---
+++ src/EEADemo.java
@@ -0,0 +1,25 @@
+public class EEADemo {
+
+    public static void main(String[] args) {
+
+        EEAlgorithmus Test = new EEAlgorithmus(24231, 1189);
+        Test.printGGT();
+
+        EEAlgorithmus Test2 = new EEAlgorithmus(1773, 1337);
+        Test2.printGGT();
+
+        EEAlgorithmus Test3 = new EEAlgorithmus(45, 36);
+        Test3.printGGT();
+
+        EEAlgorithmus Test4 = new EEAlgorithmus(94133, 91);
+        Test4.printGGT();
+
+        EEAlgorithmus Test5 = new EEAlgorithmus(778883, 91);
+        Test5.printGGT();
+
+        EEAlgorithmus Test6 = new EEAlgorithmus(11475, 7339);
+        Test6.printGGT();
+
+    }
+
+}
---
+++ src/EEAlgorithmus.java
@@ -0,0 +1,37 @@
+public class EEAlgorithmus {
+
+    int hauptzahl;
+    int modulo;
+
+    public EEAlgorithmus(int hauptzahl, int modulo) {
+        this.hauptzahl = hauptzahl;
+        this.modulo = modulo;
+    }
+
+    public int ggTBestimmung(){
+        int divident = this.hauptzahl;
+        int divisor = this.modulo;
+        int rest = divident % divisor;
+
+        while ((divident % divisor) != 0){
+                rest = divident % divisor;
+                divident = divisor;
+                divisor = rest;
+        }
+        return rest;
+    }
+
+    public void printGGT(){
+        System.out.println("ggT(" + this.hauptzahl + ", " + this.modulo + ") = " + this.ggTBestimmung());
+    }
+
+}
+
+
+    /*
+       1. DivErgebnis = Zahl / Modulo
+       2. Rest = Zahl -  Modulo * DivErgebnis
+       Rest != 0?
+       Zahl = Modulo
+       Modulo = DivErgebnis
+    */
